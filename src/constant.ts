0cce04aff32969fe4a6cc63ce6de33961d1ca7a1
thirzq <thirzahmad@gmail.com>
2025-02-14 16:40:56 +0700

    to lower case

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-const isProduction = import.meta.env.VITE_PRODUCTION_MODE == "True";
+const isProduction = import.meta.env.VITE_PRODUCTION_MODE?.toLowerCase() === "true";
 export const apiBaseUrl = isProduction ? "https://ragreader-d3rr7.ondigitalocean.app" : "http://localhost:8000";
