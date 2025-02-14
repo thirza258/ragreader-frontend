1d9c9710265af36149f1b4de6e24ffa13bd95ac8
thirzq <thirzahmad@gmail.com>
2025-02-14 16:36:01 +0700

    add

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,2 @@
-
-
-const isProduction = import.meta.env.VITE_PRODUCTION_MODE === "True";
+const isProduction = import.meta.env.VITE_PRODUCTION_MODE == "True";
 export const apiBaseUrl = isProduction ? "https://ragreader-d3rr7.ondigitalocean.app" : "http://localhost:8000";
